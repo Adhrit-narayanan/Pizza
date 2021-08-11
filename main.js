ca0be0853bcb477829a92972d729a2448d9e0e4d
Adhrit-narayanan <84670212+Adhrit-narayanan@users.noreply.github.com>
2021-08-11 19:35:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function showpizza(){
+    document.getElementById("pizza1").style.display="inline-block";
+    document.getElementById("pizza2").style.display="inline-block";
+    document.getElementById("pizza3").style.display="inline-block";
+    document.getElementById("pizza4").style.display="inline-block";
+    document.getElementById("pizza5").style.display="inline-block";
+}
